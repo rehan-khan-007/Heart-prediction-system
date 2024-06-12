f1ffb4d697901576500b6bf1095ccc03e6063d57
rehan-khan-007 <bro39404k@gmail.com>
2024-06-12 11:17:28 +0530

    Added some photos

--- main.py
+++ main.py
@@ -27,11 +27,13 @@ root.geometry("1450x730+60+80")
 root.resizable(False, False)
 root.config(bg=background)
 
+# Changed the icon of tkinter as heart 
 icon_path = "spirit_love_like_valentine_romance_soul_heart_game_icon_262424.ico"
 root.iconbitmap(icon_path)
 
+# Header Section 2
+logo = PhotoImage(file="Images/Header1.jpg")
+myimage = Label(image=logo, bg=background)
+myimage.place(x=0, y=0)
 
-# Add widgets and functionality here as needed
-
-# Start the main event loop
 root.mainloop()
